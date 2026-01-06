ce365219e5f4b1fe5c7d97f08540bdac62070582
Paweł Mstowski <pawel.mstowski@gmail.com>
2026-01-06 18:58:04 +0100

    Basic list view in tui

--- internal/tui/main.go
+++ internal/tui/main.go
@@ -1,59 +1,65 @@
 package tui
 
 import (
+	"fmt"
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/mustafmst/taskerr/internal/data"
+	"github.com/mustafmst/taskerr/internal/data/tasks"
 )
 
 type MainWindowModel struct {
 	width  int
 	height int
-	t      []TaskModel
+	t      []tasks.Task
 	s      *data.Service
 }
 
-func NewMainWindowModel(s *data.Service) *MainWindowModel {
-	return &MainWindowModel{s: s}
+func NewMainWindowModel(s *data.Service) MainWindowModel {
+	return MainWindowModel{s: s}
+}
+
+func (m MainWindowModel) LoadTasks() tea.Msg {
+	taskslist, err := m.s.TasksRepo.GetAll()
+	if err != nil {
+		log.Fatalf("Error retrieving tasks: %v", err)
+	}
+	return tasksLoadedMsg{t: taskslist}
+}
+
+type tasksLoadedMsg struct {
+	t []tasks.Task
 }
 
 // View implements tea.Model.
-func (m *MainWindowModel) View() string {
-	windowStyle := lipgloss.NewStyle().
-		Align(lipgloss.Left).
-		Width(m.width - 4). // Adjust for padding
-		Height(m.height - 4).
-		Border(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color("2")).
-		Padding(1)
+func (m MainWindowModel) View() string {
+	style := lipgloss.NewStyle().
+		Align(lipgloss.Center).
+		Width(m.width).Height(m.height)
 
-	var taskViews string
+	body := "Welcome to taskerr tui mode. Press ctrl+c to exit.\n\n\n"
 	for _, task := range m.t {
-		taskViews += task.View() + "\n"
+		body += fmt.Sprintf("- [%v] %s\n", task.State, task.Description)
 	}
 
-	return windowStyle.Render(taskViews)
+	return style.Render(body)
 }
 
-func (m *MainWindowModel) Init() tea.Cmd {
-	taskslist, err := m.s.TasksRepo.GetAll()
-	if err == nil {
-		log.Fatalf("Error retrieving tasks: %v", err)
-	}
-	m.t = make([]TaskModel, 0)
-	for _, t := range taskslist {
-		m.t = append(m.t, NewTaskModel(t))
-	}
-	return nil
+func (m MainWindowModel) Init() tea.Cmd {
+	return m.LoadTasks
 }
 
-func (m *MainWindowModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m MainWindowModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tasksLoadedMsg:
+		m.t = msg.t
+		return m, nil
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
+		return m, nil
 	case tea.KeyMsg:
 		if msg.String() == "ctrl+c" {
 			return m, tea.Quit
